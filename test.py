80304a6b8574c468b32956f99e4b27af72d35580
aJakaGithub <77320412+aJakaGithub@users.noreply.github.com>
2021-01-12 14:48:16 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+thresholds = [('low', 0),('medium',0.5)]
+thresholds = pd.DataFrame(thresholds)
+print(thresholds)
+
+prob = pd.Series([1,2,3,4,5,6])
+
+for p in prob:
+        print("prob start")
+        for _, row in thresholds.iterrows():
+                #print(row[0])
+                print(p,"--",row[1])
+
+        print("prob done")
+
+
+
